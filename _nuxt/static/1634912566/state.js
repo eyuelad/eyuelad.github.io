377a6560ad4cf47efdd7611bbf1a26c93f5a9df5
Eyuel Solomon <eyuelsolomonad@gmail.com>
2021-10-22 20:44:09 +0300

    Version 1.10: Dynamic theme

--- _nuxt/static/1634912566/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634912566",layout:"default",error:a,serverRendered:true,routePath:b,config:{baseUrl:"https:\u002F\u002Feyuelad.github.io\u002F",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1de6b0e5"}}}}(null,"\u002F"));
