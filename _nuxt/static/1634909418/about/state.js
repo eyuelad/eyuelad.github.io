22608fee769cc0afd2a43b61d4fb39cad0ca6c0f
Eyuel Solomon <eyuelsolomonad@gmail.com>
2021-10-22 16:38:04 +0300

    Version 1

---
+++ _nuxt/static/1634909418/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634909418",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{baseUrl:"https:\u002F\u002Feyuelad.github.io\u002F",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bdf33acc"}}}}(null));
